4462e8ff48f9a1243cf6692d2c739bc319ea7563
himanshubathamx2 <112793814+himanshubathamx2@users.noreply.github.com>
2023-07-29 23:48:44 +0530

    added

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUrl = 'mongodb://localhost:27017'
-const connectToMongo = () => {
-  mongoose.connect(mongoUrl, () => {
-    console.log("connected to mongo")
-  })
-}
-
-module.exports = connectToMongo;
